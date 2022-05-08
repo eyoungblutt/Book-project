04d391cc8867a00333abbb4287bcc0dace6d7368
Erin Youngblutt <eyoungblutt@example.com>
2022-05-08 12:42:46 +1000

    Linked API and started to map out DOM and event listener

---
+++ DOM.js
@@ -0,0 +1,12 @@
+// find you button that you want to apply the event listener to.
+
+const searchButton = document.getElementsByClassName(
+  "searchEngine__buttons__search-button"
+);
+
+searchButton.addEventListener("click", () => {
+  // when it is clicked have the search of the API start
+  // numberOne += numbersArr[i];
+  // Then have the answers display in the object grid.
+  // document.getElementById("number-display").innerText = numberOne;
+});
